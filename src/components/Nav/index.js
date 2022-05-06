2124f52f04ecf86b3e26ab92db587563f5141c20
patrickaregan <patrickaregan@gmail.com>
2022-05-06 17:36:03 -0500

    add tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -29,7 +29,7 @@ const Nav = () => {
             <nav>
                 <ul className="flex-row">
                 <li className="mx-2">
-                    <a href="#about">
+                    <a data-testid="about" href="#about">
                     About Me
                     </a>
                 </li>
